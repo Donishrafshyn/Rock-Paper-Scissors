3bed18f492438641c77aac226e560ab7bb2887ef
Donish Rafshyn C <donishrafshyn@gmail.com>
2025-12-02 16:14:03 +0530

    Main

---
+++ index.js
@@ -0,0 +1,42 @@
+const choices = ["rock","paper","scissors"];
+const playerchoice = document.getElementById("playerchoice");
+const computerchoice = document.getElementById("computerchoice");
+const playerscore = document.getElementById("playerscore");
+
+const computerscore = document.getElementById("computerscore");
+const resultdisplay = document.getElementById("resultdisplay");
+let playermark=0;
+let computermark=0;
+
+function playgame(play){
+    const computerselect= choices[Math.floor(Math.random() * 3)];
+    let result="";
+    
+    if(play==computerselect)
+        {
+            result="ITS'S A TIE";
+            resultdisplay.textContent=result;
+            playerchoice.textContent+=play;
+            computerchoice.textContent+=computerselect;
+        }
+    else if((play=="rock" && computerselect=="scissors") ||
+            (play=="paper" && computerselect=="rock") ||   
+            ( play=="scissors" && computerselect=="paper") )
+            {
+                result="YOU WIN";
+                resultdisplay.textContent=result;
+                playerscore.textContent=playermark++;
+
+                playerchoice.textContent=`Player : ${play}`;
+                computerchoice.textContent=`Computer : ${computerselect}`;
+            }
+    else{
+        result="YOU LOSE";
+        computerscore.textContent=computermark++;
+
+        resultdisplay.textContent=result;
+        playerchoice.textContent+=play;
+        computerchoice.textContent+=computerselect;
+    }
+    
+}
